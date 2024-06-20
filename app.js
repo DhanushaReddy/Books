b339e5b9c94bf327472133904a01123461d2adce
DhanushaReddy <51365437+DhanushaReddy@users.noreply.github.com>
2024-06-20 18:38:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,22 +8,18 @@ const app = express();
 const PORT = 3000;
 const MONGO_URI = 'mongodb://localhost:27017/book-directory';
 
-// Connect to MongoDB
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
 
-// Middleware
 app.use(bodyParser.json());
-app.use(cors());  // Enable CORS
+app.use(cors());  
 app.use('/api', bookRoutes);
 
-// Root endpoint
 app.get('/', (req, res) => {
     res.send('Welcome to the Book Directory API');
 });
 
-// Start server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
